d8db7c6a6968027268532c6a484cc226fdb4dfb9
99Valgo99 <224714804+99Valgo99@users.noreply.github.com>
2026-09-13 06:38:25 +0100

    Added the operators/topic/invited to the Channel class, the operator gets constructed inside the channel constructor, The channel's creator is directly an operator.

--- Channel.cpp
+++ Channel.cpp
@@ -3,6 +3,7 @@
 Channel::Channel(std::string name, int fd) {
     this->channelsName = name;
     this->fds_list.insert(fd);
+    this->operators.insert(fd);
 }
 
 void Channel::addClientsToChannel(int fd) {
@@ -13,6 +14,11 @@ void Channel::removeClientsFromChannel(int fd) {
     this->fds_list.erase(fd);
 }
 
+void Channel::setTopic(std::string addedTopic)
+{
+    this->topic = addedTopic;
+}
+
 const std::string& Channel::getChannelsName() const {
     return (this->channelsName);
 }
@@ -21,3 +27,22 @@ const std::set<int>& Channel::getChannelsMembers() const {
     return (this->fds_list);
 }
 
+const std::set<int>& Channel::getOperators() const {
+    return (this->operators);
+}
+
+void Channel::addOperators(int fd) {
+    this->operators.insert(fd);
+}
+
+void Channel::addInvited(int fd) {
+    this->invited.insert(fd);
+}
+
+const std::set<int>& Channel::getInvitedMembers() const {
+    return (this->invited);
+}
+
+const std::string& Channel::getTopic() const {
+    return (this->topic);
+}
--- Channel.hpp
+++ Channel.hpp
@@ -7,14 +7,29 @@
 class Channel {
 
     private:
-        std::set<int> fds_list;
+        std::string topic;
         std::string channelsName;
+        std::set<int> fds_list;
+        std::set<int> operators;
+        std::set<int> invited;
+    
     public:
+        Channel(std::string name, int fd);
+        
         void addClientsToChannel(int fd);
         void removeClientsFromChannel(int fd);
+
+        void setTopic(std::string addedTopic);
+        const std::string& getTopic() const;
         const std::string& getChannelsName() const;
         const std::set<int>& getChannelsMembers() const;
-        Channel(std::string name, int fd);
+        
+        void addOperators(int fd);
+        const std::set<int>& getOperators() const;
+
+        void addInvited(int fd);
+        const std::set<int>& getInvitedMembers() const;
+
 };
 
 
